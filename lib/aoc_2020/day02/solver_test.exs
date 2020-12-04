d33806030100b2b77249341f8a402b5e12508fd0
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2020-12-04 22:09:56 +0100

    Day 2 part 2

--- lib/aoc_2020/day02/solver_test.exs
+++ lib/aoc_2020/day02/solver_test.exs
@@ -13,5 +13,13 @@ defmodule AOC2020.Day02.SolverTest do
       assert 2 == MUT.solve(input, :first)
     end
 
+    test "should solve second task with test input from website correctly" do
+      input = [
+        "1-3 a: abcde",
+        "1-3 b: cdefg",
+        "2-9 c: ccccccccc",
+      ]
+      assert 1 == MUT.solve(input, :second)
+    end
   end
 end
