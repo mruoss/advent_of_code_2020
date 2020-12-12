9bc4a0eb3832e2ae6f3449a8903c469d0193d9be
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2020-12-12 13:56:39 +0100

    Day 12 Part 2

--- lib/aoc_2020/day12/solver.ex
+++ lib/aoc_2020/day12/solver.ex
@@ -1,6 +1,8 @@
 defmodule AOC2020.Day12.Solver do
   alias AOC2020.Day12.Vessel
-  def solve(stream, subtask) do
+  alias AOC2020.Day12.Vessel2
+
+  def solve(stream, :first) do
     stream
     |> Stream.map(&String.trim/1)
     |> Stream.map(&parse_instruction/1)
@@ -11,6 +13,19 @@ defmodule AOC2020.Day12.Solver do
     end)
     |> Enum.at(0)
     |> Vessel.manhattan_distance()
+  end
+
+  def solve(stream, :second) do
+    stream
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&parse_instruction/1)
+    |> Stream.concat([:end])
+    |> Stream.transform(%Vessel2{wp: [[10, 1]], position: [[0, 0]]}, fn
+      :end, vessel -> {[vessel], nil}
+      instr, vessel -> {[], Vessel2.exec_instr(vessel, instr)}
+    end)
+    |> Enum.at(0)
+    |> Vessel2.manhattan_distance()
   end
 
   defp parse_instruction(instr) do
--- lib/aoc_2020/day12/solver_test.exs
+++ lib/aoc_2020/day12/solver_test.exs
@@ -4,21 +4,32 @@ defmodule AOC2020.Day12.SolverTest do
   alias AOC2020.Day12.Solver, as: MUT
 
   describe "solve/2" do
-      test "should solve first task with first test input from website correctly" do
-        input = """
-        F10
-        N3
-        F7
-        R90
-        F11
-        """
-        |> String.split("\n", trim: true)
-        |> Enum.map(fn s -> s <> "\n" end)
+    test "should solve first task with first test input from website correctly" do
+      input = """
+      F10
+      N3
+      F7
+      R90
+      F11
+      """
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn s -> s <> "\n" end)
 
       assert 25 == MUT.solve(input, :first)
     end
 
     test "should solve second task with first test input from website correctly" do
+      input = """
+      F10
+      N3
+      F7
+      R90
+      F11
+      """
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn s -> s <> "\n" end)
+
+      assert 286 == MUT.solve(input, :second)
     end
   end
 end
